0017ed3936a8317f856c0bc9565062ab4130c358
Tamilmani N <ntamilmanin@gmail.com>
2025-11-13 13:08:16 +0000

    Added automated report generation and upload

--- system_report.sh
+++ system_report.sh
@@ -1,13 +1,21 @@
-#!/bin/nash
-echo "===== SYSTEM REPORT ====="
-date
-echo 
-echo "Hostname: $(hostnama)"
-echo "Uptime:"
-uptime
-echo
-echo "Disk Usage:"
-df -h
-echo 
-echo "Installed Package:"
-dpkg --get-selections | wc -l
+#!/bin/bash
+
+echo "===== DAILY SYSTEM REPORT =====" > report.txt
+date >> report.txt
+echo >> report.txt
+
+echo "🖥️ Hostname: $(hostname)" >> report.txt
+echo "⏱️ Uptime:" >> report.txt
+uptime >> report.txt
+echo >> report.txt
+
+echo "💾 Disk Usage:" >> report.txt
+df -h >> report.txt
+echo >> report.txt
+
+echo "📦 Installed Packages Count:" >> report.txt
+dpkg --get-selections | wc -l >> report.txt
+
+echo >> report.txt
+echo "===== END OF REPORT =====" >> report.txt
+
